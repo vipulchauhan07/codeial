6465d472c5d52fc7fbfb1ac8e8e908339f51294b
Vipul Chauhan <viratdev07@gmail.com>
2020-03-13 21:31:41 +0530

    Writing function in the controller

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 
 const router = express.Router();
-
-
+const homeController = require('../controllers/home_controller');
+//const practiceController = require('../controllers/home_controller');
 console.log('router loaded');
-
+router.get('/', homeController.practice);
+router.get('/', homeController.home);
 module.exports = router;
