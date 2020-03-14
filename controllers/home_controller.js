84f061ad8ac12fbf17f7cb8b7276095a64e6550f
Vipul Chauhan <viratdev07@gmail.com>
2020-03-14 06:18:15 +0530

    Render an page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,8 @@
 module.exports.home = function(req, res){
-    return res.end('<h1>Express is up for codeial</h1>');
+    
+    return res.render('home', {
+        title: 'Home'
+    });
 }
 
-module.exports.practice = function(req, res){
-    return res.end('<h1>This is for the practice sake bc practice make you perfect webdeveloper!.. hahaha!');
-    
-}
+// module.exports.actionName = function(req, res){}
