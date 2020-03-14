066c49972fd0c796278f893930083d94d3f58bb7
Vipul Chauhan <viratdev07@gmail.com>
2020-03-14 15:01:38 +0530

    Add Sign-in & Sign-up Pages And Render

--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const usersController = require('../controllers/users_controller');
 const postsController = require('../controllers/posts_controller');
 router.get('/profile', usersController.profile);
+router.get('/sign-up', usersController.signUp);
+router.get('/sign-in', usersController.signIn);
 router.get('/feed', postsController.feed);
 module.exports = router;
