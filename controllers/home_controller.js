06993ba2776cddb002e2d21715f2a794f4f3ffad
Vipul Chauhan <viratdev07@gmail.com>
2020-03-17 21:37:11 +0530

    Display Posts and Related user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,17 @@
+const Post = require('../models/post');
 module.exports.home = function(req, res){
-    
-    return res.render('home', {
-        title: 'Home Page'
+    // Post.find({}, function(err, posts){
+    //     return res.render('home', {
+    //         title: 'Codeial | Home',
+    //         posts: posts
+    //     });
+    // });
+    // populate the user of each post
+    Post.find({}).populate('user').exec(function(err, posts){
+        return res.render('home', {
+            title: 'Codeial | Home',
+            posts: posts
+        });
     });
 }
 
