c373947cff378bd6989d9e58229ccfeff197f78e
Vipul Chauhan <viratdev07@gmail.com>
2020-03-19 01:51:23 +0530

    Link users profile

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const User = require('../models/user');
 module.exports.home = function(req, res){
     // Post.find({}, function(err, posts){
     //     return res.render('home', {
@@ -16,9 +17,13 @@ module.exports.home = function(req, res){
         }
     })
     .exec(function(err, posts){
-        return res.render('home', {
+        User.find({}, function(err, users) {
+            return res.render('home', {
             title: 'Codeial | Home',
-            posts: posts
+            posts: posts,
+            all_users: users
+        });
+        
         });
     });
 }
