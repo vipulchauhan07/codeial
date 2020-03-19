9e3bd99528a26bb641938b94fe48cd1adafd7549
Vipul Chauhan <viratdev07@gmail.com>
2020-03-19 17:57:29 +0530

    Added some more Noty notifications

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -6,11 +6,11 @@ module.exports.create = async function(req, res) {
         content: req.body.content,
         user: req.user._id
         });
-
+        req.flash('success','Post published!');
         return res.redirect('back');
     } catch (err) {
-        console.log('Error', err);
-        return;
+       req.flash('error',err);
+        return res.redirect('back');
     }   
     
 }
@@ -25,13 +25,16 @@ module.exports.destroy = async function(req, res) {
             post.remove();
                 
             await Comment.deleteMany({post: req.params.id});
+
+            req.flash('success','Post and Associated comments deleted');
             return res.redirect('back');
         } else {
-        return res.redirect('back');
+            req.flash('error','You cannot delete this post.');
+            return res.redirect('back');
     }
     } catch (err) {
-        console.log('Error', err);
-        return;
+        req.flash('error',err);
+        return res.redirect('back');
     }
 
 }
