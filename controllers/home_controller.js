8dfc2e9842306b475f8d3ea7c3e08ff89b70ad7f
Vipul Chauhan <viratdev07@gmail.com>
2020-03-19 16:59:55 +0530

    Create Async Await Code

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,31 +1,37 @@
-const Post = require('../models/post');
-const User = require('../models/user');
-module.exports.home = function(req, res){
-    // Post.find({}, function(err, posts){
-    //     return res.render('home', {
-    //         title: 'Codeial | Home',
-    //         posts: posts
-    //     });
-    // });
+const Post = require("../models/post");
+const User = require("../models/user");
+
+module.exports.home = async function(req, res) {
+ try {
     // populate the user of each post
-    Post.find({})
-    .populate('user')
+    let posts = await Post.find({})
+    .populate("user")
     .populate({
-        path: 'comments',
-        populate:{
-            path: 'user'
+        path: "comments",
+        populate: {
+        path: "user"
         }
-    })
-    .exec(function(err, posts){
-        User.find({}, function(err, users) {
-            return res.render('home', {
-            title: 'Codeial | Home',
-            posts: posts,
-            all_users: users
-        });
-        
-        });
     });
+
+    let users = await User.find({});
+
+    return res.render("home", {
+    title: "Codeial | Home",
+    posts: posts,
+    all_users: users
+    });
+ } catch (err) {
+     console.log('Error', err);
+     return;
+ }
+ 
 }
 
 // module.exports.actionName = function(req, res){}
+
+// using then
+// Post.find({}).populate('comment').then();
+
+// let posts = Post.find({}).populate('comment').exec();
+
+// posts.then();
