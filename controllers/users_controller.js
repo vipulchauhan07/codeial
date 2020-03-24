720df3e7ceca0f1ac10833e3426d63769ce30e5d
Vipul Chauhan <viratdev07@gmail.com>
2020-03-24 19:36:06 +0530

    File Upload

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -24,14 +24,15 @@ module.exports.update = async function (req, res) {
   // }
   if (req.user.id == req.params.id) {
     try {
-        let user = await User.findById(req.params.id);
-		User.uploadedAvatar(req, res, function(err){
+
+            let user = await User.findById(req.params.id);
+		    User.uploadedAvatar(req, res, function(err){
 			if (err){console.log(">>>>Multer Error:" ,err);}
 			user.name  = req.body.name;
 			user.email = req.body.email;
 			if(req.file) {
                 if(user.avatar) {
-                    fs.unlinkSync(path.join(__dirname + '..' + user.avatar));
+                    fs.unlinkSync(path.join(__dirname, '..', user.avatar));
                 }
 				// this is saving the path of uploaded file into the avatar field in the user
 				user.avatar = User.avatarPath + '/' + req.file.filename;
